baf854756b1af13acbca2290438af06b6ada17ef
whiitex <pasquale03.bianco@gmail.com>
2024-07-26 23:04:14 +0200

    feat: mouse tracker auto-initilization when OutOfTrack and minus sign impl

--- src/mouse_tracker.rs
+++ src/mouse_tracker.rs
@@ -1,3 +1,4 @@
+use tracing_subscriber::fmt::layer;
 use crate::run_popup;
 
 #[derive(Debug)]
@@ -6,6 +7,7 @@ pub enum TrackingResult {
     InCurrentRect,      // mouse is now in the current rect  / continue
     InNextRect,         // mouse is now in the next rect     / continue
     FinishedRectShape,  // mouse is now in the last rect!    / go to next shape
+    FinishedMinusShape,  // mouse is now in the last rect!    / backup!
     OutOfTrack,         // mouse is now out of given track   / reset all
 }
 
@@ -34,131 +36,34 @@ impl Rectangular {
 
 #[derive(Debug)]
 pub struct MouseTracker {
+    id: u8, // 0: RECT, 1: MINUS
     width: i32,
     height: i32,
     track: Vec<Rectangular>,
     current_index: usize,  // index in track vector
 }
 
-
-pub fn point_in_rect(p: &Point, r: &Rectangular) -> bool {
-    return p.x >= r.top_sx.x &&
-        p.x <= r.bot_rx.x &&
-        p.y >= r.top_sx.y &&
-        p.y <= r.bot_rx.y;
-}
-
-
 impl MouseTracker {
     pub fn new(w: i32, h: i32) -> Self {
-        let mut track: Vec<Rectangular> = Vec::<Rectangular>::new();
-
-        let min_cells = 10;
-        let size = (i32::min(h, w) + min_cells - 1) / min_cells;
-        let limit = size * 2 / 3;
-
-
-        // center Oxy in top left corner
-        {
-            // vertical, left line
-            let mut i = 0;
-            while i < h {
-                if h < i + size || h - (i + size) < limit {
-                    track.push(Rectangular {
-                        top_sx: Point { x: -10, y: i },
-                        bot_rx: Point { x: size-1, y: h + 10 },
-                    });
-                    i = h;
-                } else {
-                    track.push(Rectangular {
-                        top_sx: Point { x: -10, y: i },
-                        bot_rx: Point {
-                            x: size-1,
-                            y: i32::min(h +10, i + size-1),
-                        },
-                    });
-                    i += size;
-                }
-            }
-
-            // horizontal, bottom line
-            i = size;
-            while i < w {
-                if w < i + size || w - (i + size) < limit {
-                    track.push(Rectangular {
-                        top_sx: Point { x: i, y: h - size },
-                        bot_rx: Point { x: w +10, y: h +10 },
-                    });
-                    i = w;
-                } else {
-                    track.push(Rectangular {
-                        top_sx: Point { x: i, y: h - size },
-                        bot_rx: Point {
-                            x: i32::min(w +10, i + size-1),
-                            y: h +10
-                        },
-                    });
-                    i += size;
-                }
-            }
-
-            // vertical, right line
-            let mut i = h - size -1;
-            while i > 0 {
-                if i < size || i - size < limit {
-                    track.push(Rectangular {
-                        top_sx: Point { x: w - size, y: -10 },
-                        bot_rx: Point { x: w +10, y: i },
-                    });
-                    i = 0;
-                } else {
-                    track.push(Rectangular {
-                        top_sx: Point {
-                            x: w - size,
-                            y: if i >= size-1 { i - size +1 } else { -10 } },
-                        bot_rx: Point { x: w +10, y: i },
-                    });
-                    i -= size;
-                }
-            }
-
-            // horizontal, top line
-            i = w - size -1;
-            while i > 0 {
-                if i < size || i - size < limit {
-                    track.push(Rectangular {
-                        top_sx: Point { x: -10, y: -10 },
-                        bot_rx: Point { x: i, y: size -1 },
-                    });
-                    i = 0;
-                } else {
-                    track.push(Rectangular {
-                        top_sx: Point {
-                            x: if i >= size - 1 { i - size + 1 } else { -10 },
-                            y: -10 },
-                        bot_rx: Point { x: i, y: size -1 },
-                    });
-                    i -= size;
-                }
-            }
-        }
-
         MouseTracker {
+            id: 0,
             width: w,
             height: h,
-            track: track,
+            track: init_rect(w, h),
             current_index: 0,
         }
+    }
+
+    pub fn re_init(&mut self) -> () {
+        self.id = 0;
+        self.current_index = 0;
+        self.track = init_rect(self.width, self.height);
     }
 
     pub fn track_point(&mut self, p: Point) -> TrackingResult {
         let curr_rect: Rectangular = self.track[self.current_index].clone();
 
         // mouse still in current rect
-        if point_in_rect(&p, &curr_rect) && self.current_index == self.track.len() - 1 {
-            return TrackingResult::FinishedRectShape;
-        }
-
         if point_in_rect(&p, &curr_rect) {
             return TrackingResult::InCurrentRect;
 
@@ -170,12 +75,174 @@ impl MouseTracker {
         } else if self.current_index + 1 < self.track.len() && point_in_rect(&p, &self.track[self.current_index + 1]) {
             self.current_index += 1;
             // in last rect
-            if self.current_index + 1 == self.track.len() {
-                return TrackingResult::FinishedRectShape;
+            if self.current_index != self.track.len() - 1 {
+                return TrackingResult::InNextRect;
                 // not the last rect
-            } else { return TrackingResult::InNextRect; }
+            } else {
+
+                if self.id == 0 { /// RECT
+                    self.id = 1;
+                    self.current_index = 0;
+                    self.track = init_minus(self.width, self.height);
+                    return TrackingResult::FinishedRectShape;
+                } else {          /// MINUS
+                    self.id = 0;
+                    self.current_index = 0;
+                    self.track = init_rect(self.width, self.height);
+                    return TrackingResult::FinishedMinusShape;
+                }
+            }
         }
 
-        return TrackingResult::OutOfTrack;
+        // re-init and return out of track
+        if self.id == 0 {
+            self.id = 0;
+            self.current_index = 0;
+            self.track = init_rect(self.width, self.height);
+        }
+
+        TrackingResult::OutOfTrack
     }
 }
+
+pub fn point_in_rect(p: &Point, r: &Rectangular) -> bool {
+    return p.x >= r.top_sx.x &&
+        p.x <= r.bot_rx.x &&
+        p.y >= r.top_sx.y &&
+        p.y <= r.bot_rx.y;
+}
+
+fn init_rect(w: i32, h: i32) -> Vec<Rectangular> {
+    let mut track: Vec<Rectangular> = Vec::<Rectangular>::new();
+
+    let min_cells = 10;
+    let size = (i32::min(h, w) + min_cells - 1) / min_cells;
+    let limit = size * 2 / 3;
+
+
+    // Oxy in top left corner
+    {
+        // vertical, left line
+        let mut i = 0;
+        while i < h {
+            if h < i + size || h - (i + size) < limit {
+                track.push(Rectangular {
+                    top_sx: Point { x: -10, y: i },
+                    bot_rx: Point { x: size-1, y: h + 10 },
+                });
+                i = h;
+            } else {
+                track.push(Rectangular {
+                    top_sx: Point { x: -10, y: i },
+                    bot_rx: Point {
+                        x: size-1,
+                        y: i32::min(h +10, i + size-1),
+                    },
+                });
+                i += size;
+            }
+        }
+
+        // horizontal, bottom line
+        i = size;
+        while i < w {
+            if w < i + size || w - (i + size) < limit {
+                track.push(Rectangular {
+                    top_sx: Point { x: i, y: h - size },
+                    bot_rx: Point { x: w +10, y: h +10 },
+                });
+                i = w;
+            } else {
+                track.push(Rectangular {
+                    top_sx: Point { x: i, y: h - size },
+                    bot_rx: Point {
+                        x: i32::min(w +10, i + size-1),
+                        y: h +10
+                    },
+                });
+                i += size;
+            }
+        }
+
+        // vertical, right line
+        let mut i = h - size -1;
+        while i > 0 {
+            if i < size || i - size < limit {
+                track.push(Rectangular {
+                    top_sx: Point { x: w - size, y: -10 },
+                    bot_rx: Point { x: w +10, y: i },
+                });
+                i = 0;
+            } else {
+                track.push(Rectangular {
+                    top_sx: Point {
+                        x: w - size,
+                        y: if i >= size-1 { i - size +1 } else { -10 } },
+                    bot_rx: Point { x: w +10, y: i },
+                });
+                i -= size;
+            }
+        }
+
+        // horizontal, top line
+        i = w - size -1;
+        while i > 0 {
+            if i < size || i - size < limit {
+                track.push(Rectangular {
+                    top_sx: Point { x: -10, y: -10 },
+                    bot_rx: Point { x: i, y: size -1 },
+                });
+                i = 0;
+            } else {
+                track.push(Rectangular {
+                    top_sx: Point {
+                        x: if i >= size - 1 { i - size + 1 } else { -10 },
+                        y: -10 },
+                    bot_rx: Point { x: i, y: size -1 },
+                });
+                i -= size;
+            }
+        }
+    }
+
+    track
+}
+
+fn init_minus(w: i32, h: i32) -> Vec<Rectangular> {
+    let mut track: Vec<Rectangular> = Vec::<Rectangular>::new();
+
+    let min_cells = 10;
+    let size = (i32::min(h, w) + min_cells - 1) / min_cells;
+    let limit = size * 2 / 3;
+
+    // first rect
+    track.push(Rectangular {
+        top_sx: Point { x: -10, y: -10 },
+        bot_rx: Point { x: size -1, y: h +10 },
+    });
+
+    // horizontal, bottom line
+    let mut i = size;
+    while i < w {
+        if w < i + size || w - (i + size) < limit {
+            track.push(Rectangular {
+                top_sx: Point { x: i, y: -10 },
+                bot_rx: Point { x: w +10, y: h +10 },
+            });
+            i = w;
+        } else {
+            track.push(Rectangular {
+                top_sx: Point { x: i, y: -10 },
+                bot_rx: Point {
+                    x: i32::min(w +10, i + size-1),
+                    y: h +10
+                },
+            });
+            i += size;
+        }
+    }
+
+    track
+}
+
+
